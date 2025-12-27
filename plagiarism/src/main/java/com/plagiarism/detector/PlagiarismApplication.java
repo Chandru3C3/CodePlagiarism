7c7a1c6ccaecfd7f342b384453de0d08511a5b5c
chandru3c3 <chandruravik3@gmail.com>
2025-12-27 22:32:46 +0530

    CROS config changes

--- plagiarism/src/main/java/com/plagiarism/detector/PlagiarismApplication.java
+++ plagiarism/src/main/java/com/plagiarism/detector/PlagiarismApplication.java
@@ -2,38 +2,12 @@ package com.plagiarism.detector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.plagiarism.detector.controller", "com.plagiarism.detector.entity",
-		"com.plagiarism.detector.repository", "com.plagiarism.detector.service", "com.plagiarism.detector.dto",
-		"com.plagiarism.detector.dto" })
 public class PlagiarismApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlagiarismApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/cpd/**") // Apply CORS to all /api endpoints
-						.allowedOrigins("http://localhost:4200", "https://codeplagiarism.netlify.app",
-								"http://localhost:3000", "http://127.0.0.1:4200") // Allow your
-																					// Angular
-						// origins
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow all common methods
-						.allowedHeaders("*") // Allow all headers
-						.allowCredentials(true) // Allow cookies and authentication headers
-						.maxAge(3600); // How long the preflight response can be cached (in seconds)
-			}
-		};
-
 	}
 
 }
